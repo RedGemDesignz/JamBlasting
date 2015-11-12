58cd4e60d7813998a272dbc1f341ac9922cd7a5e
Lundieee <deoncoke@rocketmail.com>
2015-11-11 22:44:36 -0600

    Merge remote-tracking branch 'origin/master'
    Conflicts:
    	.idea/misc.xml

--- app/src/main/java/redgemdesignz/jamblastcomics/Comics_Scripts.java
+++ app/src/main/java/redgemdesignz/jamblastcomics/Comics_Scripts.java
@@ -3,11 +3,17 @@ package redgemdesignz.jamblastcomics;
 import android.os.Bundle;
 import android.support.design.widget.FloatingActionButton;
 import android.support.design.widget.Snackbar;
+import android.support.v4.view.MotionEventCompat;
 import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.Toolbar;
+import android.util.Log;
 import android.view.View;
+import android.view.MotionEvent;
+
 
 public class Comics_Scripts extends AppCompatActivity {
+
+    String DEBUG_TAG = "test rund";
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -24,6 +30,32 @@ public class Comics_Scripts extends AppCompatActivity {
                         .setAction("Action", null).show();
             }
         });
+    }
+
+    public boolean onTouchEvent(MotionEvent event){
+
+        int action = MotionEventCompat.getActionMasked(event);
+
+        switch(action) {
+            case (MotionEvent.ACTION_DOWN) :
+                Log.d(DEBUG_TAG, "Action was DOWN");
+                return true;
+            case (MotionEvent.ACTION_MOVE) :
+                Log.d(DEBUG_TAG,"Action was MOVE");
+                return true;
+            case (MotionEvent.ACTION_UP) :
+                Log.d(DEBUG_TAG,"Action was UP");
+                return true;
+            case (MotionEvent.ACTION_CANCEL) :
+                Log.d(DEBUG_TAG,"Action was CANCEL");
+                return true;
+            case (MotionEvent.ACTION_OUTSIDE) :
+                Log.d(DEBUG_TAG,"Movement occurred outside bounds " +
+                        "of current screen element");
+                return true;
+            default :
+                return super.onTouchEvent(event);
+        }
     }
 
     @Override
